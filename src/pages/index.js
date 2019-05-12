f13a04a3f05e256d75807eda4051ee1bb74e9d51
Ryan Jones <ryanjones33@gmail.com>
2019-05-11 22:01:02 -0700

    Add text file

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi, I'm Ryan Jones</h1>
+    <p>Welcome to my site.</p>
+    
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
